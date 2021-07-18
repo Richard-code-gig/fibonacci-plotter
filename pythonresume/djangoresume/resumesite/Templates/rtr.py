e6759ade3be59af34410f97bf716bd1efa474dc0
Richard-code-gig <69102122+Richard-code-gig@users.noreply.github.com>
2021-07-18 20:54:53 +0100

    Delete pythonresume/djangoresume directory

--- pythonresume/djangoresume/resumesite/Templates/rtr.py
+++
@@ -1,5 +0,0 @@
-
-def replace_str (x):
-    print(x.replace('warden', 'manager'))
-a = 'A warden does a lot of jobs'
-replace_str(a)
--- pythonresume/djangoresume/resumesite/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResumesiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resumesite'
